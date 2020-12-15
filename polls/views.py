0c3e82631fca08a450b8a7ec2e3ca57570879a0d
Johann Alberts <alberts.johann@gmail.com>
2020-12-15 14:37:10 +0000

    Add skeleton views for detail, results, and vote with respective urls

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('Hello, world. You\'re at the polls index.')
 
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking att he results of question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")

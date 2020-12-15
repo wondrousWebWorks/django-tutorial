6b8f9c4ef369aaf8239b17bf958e6b26cdfb0f7d
Johann Alberts <alberts.johann@gmail.com>
2020-12-15 16:17:22 +0000

    Convert polls views to render templates and use get_object_or_404 in details view

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse('Hello, world. You\'re at the polls index.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        question: question,
+    }
+    return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking att he results of question {question_id}")
+    return HttpResponse(f"You're looking at the results of question {question_id}")
 
 def vote(request, question_id):
     return HttpResponse(f"You're voting on question {question_id}")

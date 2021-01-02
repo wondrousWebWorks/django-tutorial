435a85382decec30b42ae02da836e469e22ae2bc
Johann Alberts <alberts.johann@gmail.com>
2021-01-02 14:11:15 +0000

    Add a date list filter and search field to Question table in admin

--- polls/admin.py
+++ polls/admin.py
@@ -13,6 +13,10 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date Information', {'fields': ['pub_date']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
